c3d8585952e95f5f49411c6e95834e7175ecbb16
Lyra Darling <darling.lyra@student.greenriver.edu>
2022-01-13 17:32:40 -0500

    controller added

---
+++ index.php
@@ -0,0 +1,20 @@
+<!--this is the controller -->
+<?php
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once ('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+
+//define a default root
+$f3->route("GET /", function(){
+    echo"<h1> Hello, World!</h1>"; //this is a view
+});
+
+//run fat free
+$f3->run();
